14f0fe732b1ccbc352d1ec4e2ffc19f924a09acb
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-23 15:26:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+def gen_combinations( N, R ) : 
+  allcombinations = [] 
+  # Your code to generate all the combinations goes in here.
+  return allcombinations
+  
+
+# This allows you to test your function
+print( gen_combinations( 5, 3) )
